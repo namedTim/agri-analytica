0e497d7c70638cead8b14002f1c2850ee857d916
Tim <46692491+namedTim@users.noreply.github.com>
2024-01-14 10:34:31 +0100

    secure API supported

---
+++ AspnetCoreFull/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using AspnetCoreFull.Data;
+using AspnetCoreFull.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreFull.Controllers;
+
+[Route("api/v1/User")]
+[ApiController]
+[ApiKeyAuth]
+public class ApiController : ControllerBase
+{
+  private readonly AnalyticaContext _context;
+
+  public ApiController(AnalyticaContext context)
+  {
+    _context = context;
+  }
+
+  // Test API endpoint
+  [HttpGet("Test")]
+  public IActionResult TestApi()
+  {
+    return Ok("API is working correctly.");
+  }
+}
